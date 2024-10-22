3207eef2cd2662d0ca2ecb37a6cbd5656c995ccc
tarsuniversefrontend1 <tarsuniversefrontend1@gmail.com>
2024-10-22 18:05:11 +0600

    deploy 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["test.tarsuniverse.com"],
-    unoptimized: true,
-  },
-  output: "export",
-  reactStrictMode: true,
-  trailingSlash: true,
-  swcMinify: true,
-  distDir: "build",
+  // images: {
+  //   domains: ["test.tarsuniverse.com"],
+  //   unoptimized: true,
+  // },
+  // output: "export",
+  // reactStrictMode: true,
+  // trailingSlash: true,
+  // swcMinify: true,
+  // distDir: "build",
 };
 
 export default nextConfig;
